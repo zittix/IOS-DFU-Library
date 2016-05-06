a73ae841881642897308b8486ba76eaa76743a67
Mathieu Monney <mathieu.monney@vidinoti.com>
2016-05-06 17:02:37 +0200

    Removed depedencies to use carthage

--- DFULibrary/DFUStreamZip.swift
+++ DFULibrary/DFUStreamZip.swift
@@ -20,7 +20,7 @@
 * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-// import EVReflection
+import EVReflection
 
 // Errors
 internal enum DFUStreamZipError : ErrorType {
